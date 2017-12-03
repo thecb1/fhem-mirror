8c885fb8b58a312ac8006774c1fe0a8ef0f7a7f2
cb1 <kontakt@it-buchinger.de>
2017-12-03 12:50:57 +0100

    Replace Iconv with native perl encode()

--- fhem/FHEM/77_UWZ.pm
+++ fhem/FHEM/77_UWZ.pm
@@ -53,7 +53,6 @@ eval "use HTTP::Request;1" or $missingModul .= "HTTP::Request ";
 eval "use HTML::Parser;1" or $missingModul .= "HTML::Parser ";
 eval "use JSON;1" or $missingModul .= "JSON ";
 eval "use Encode::Guess;1" or $missingModul .= "Encode::Guess ";
-eval "use Text::Iconv;1" or $missingModul .= "Text::Iconv ";
 
 require 'Blocking.pm';
 require 'HttpUtils.pm';
@@ -772,7 +771,6 @@ sub UWZ_Run($) {
     my $response = UWZ_JSONAcquire($hash,$hash->{URL}); 
 
     UWZ_Log $hash, 5, length($response)." characters captured";
-    my $converter = Text::Iconv->new("windows-1252","UTF-8");
     my $uwz_warnings = JSON->new->ascii->decode($response);
     my $enc = guess_encoding($uwz_warnings);
 
@@ -938,10 +936,10 @@ sub UWZ_Run($) {
         $message .= "Warn_".$i."_levelName|".$single_warning->{'payload'}{'levelName'}."|";
         
         UWZ_Log $hash, 4, "Warn_".$i."_AltitudeMin: ".$enc->decode($single_warning->{'payload'}{'altMin'});
-        $message .= "Warn_".$i."_AltitudeMin|".$converter->convert($single_warning->{'payload'}{'altMin'})."|";
+        $message .= "Warn_".$i."_AltitudeMin|".encode("UTF-8", decode("iso-8859-1", $single_warning->{'payload'}{'altMin'}))."|";
 
         UWZ_Log $hash, 4, "Warn_".$i."_AltitudeMax: ".$enc->decode($single_warning->{'payload'}{'altMax'});
-        $message .= "Warn_".$i."_AltitudeMax|".$converter->convert($single_warning->{'payload'}{'altMax'})."|";
+        $message .= "Warn_".$i."_AltitudeMax|".encode("UTF-8", decode("iso-8859-1", $single_warning->{'payload'}{'altMax'}))."|";
 
         my $uclang = "EN";
         if (AttrVal( $name, 'lang',undef) ) {
@@ -955,10 +953,10 @@ sub UWZ_Run($) {
             }
         }
         UWZ_Log $hash, 4, "Warn_".$i."_LongText: ".$enc->decode($single_warning->{'payload'}{'translationsLongText'}{$uclang});
-        $message .= "Warn_".$i."_LongText|".$converter->convert($single_warning->{'payload'}{'translationsLongText'}{$uclang})."|";
+        $message .= "Warn_".$i."_LongText|".encode("UTF-8", decode("iso-8859-1", $single_warning->{'payload'}{'translationsLongText'}{$uclang}))."|";
             
         UWZ_Log $hash, 4, "Warn_".$i."_ShortText: ".$enc->decode($single_warning->{'payload'}{'translationsShortText'}{$uclang});
-        $message .= "Warn_".$i."_ShortText|".$converter->convert($single_warning->{'payload'}{'translationsShortText'}{$uclang})."|";
+        $message .= "Warn_".$i."_ShortText|".encode("UTF-8", decode("iso-8859-1", $single_warning->{'payload'}{'translationsShortText'}{$uclang}))."|";
 
 ###
         if (AttrVal( $name, 'localiconbase',undef) ) {
@@ -1530,7 +1528,7 @@ sub UWZSearchAreaID($$) {
    A maximum of 10 thunderstorm warnings will be served.
    Additional the module provides a few functions to create HTML-Templates which can be used with weblink.
    <br>
-   <i>The following Perl-Modules are used within this module: HTTP::Request, LWP::UserAgent, JSON, Encode::Guess, Text::Iconv und HTML::Parse</i>.
+   <i>The following Perl-Modules are used within this module: HTTP::Request, LWP::UserAgent, JSON, Encode::Guess and HTML::Parse</i>.
    <br/><br/>
    <b>Define</b>
    <ul>
@@ -1950,7 +1948,7 @@ sub UWZSearchAreaID($$) {
    Es werden maximal 10 Standortbezogene Unwetterwarnungen zur Verfügung gestellt.
    Weiterhin verfügt das Modul über HTML-Templates welche als weblink verwendet werden können.
    <br>
-   <i>Es nutzt die Perl-Module HTTP::Request, LWP::UserAgent, JSON, Encode::Guess, Text::Iconv und HTML::Parse</i>.
+   <i>Es nutzt die Perl-Module HTTP::Request, LWP::UserAgent, JSON, Encode::Guess und HTML::Parse</i>.
    <br/><br/>
    <b>Define</b>
    <ul>
